e922f86a1f327613cd0b1c6ebced4bd2524d935c
doulosjj1992-debug <doulosjj1992@gmail.com>
2025-09-22 17:45:58 -0500

    Let / be handled by vercel.json rewrite; remove redirect route; add minimal pages/404

--- src/app/route.ts
+++
@@ -1,4 +0,0 @@
-export async function GET() {
-  // minimal route so Next can build; redirects root to your real homepage
-  return Response.redirect('https://www.deshazogroup.com/index.html', 308);
-}
