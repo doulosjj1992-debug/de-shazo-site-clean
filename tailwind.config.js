9caf9494cb28f1d87b82342fcbb8de9588fc88d7
doulosjj1992-debug <doulosjj1992@gmail.com>
2025-09-22 16:00:11 -0500

    Fix Tailwind global CSS location and import; ensure configs exist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{ts,tsx,js,jsx,md,mdx}'],
+  content: ['./src/**/*.{ts,tsx,js,jsx,md,mdx}', './public/**/*.html'],
   theme: { extend: {} },
   plugins: [],
 };
